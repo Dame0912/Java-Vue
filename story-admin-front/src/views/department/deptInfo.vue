6f5a0379888297f80fa5ff7e68ff948385356910
Dame <dame0912@foxmail.com>
2020-04-26 15:38:45 +0800

    页面Bug处理

--- story-admin-front/src/views/department/deptInfo.vue
+++ story-admin-front/src/views/department/deptInfo.vue
@@ -23,7 +23,7 @@
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button @click="dialogFormVisible = false; dept={}, parentId=''">取 消</el-button>
         <el-button type="primary" @click="saveDept">确 定</el-button>
       </div>
     </el-dialog>
